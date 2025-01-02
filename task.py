e0e80cba9fc698a2434586a57d07e83d6bcea7c9
Maxim <kmaxusk@gmail.com>
2025-01-02 11:33:22 +0200

    Update task.py
    in-progress

--- task.py
+++ task.py
@@ -30,7 +30,7 @@ class Phone(Field):
 class Birthday(Field):
     def __init__(self, value):
         try:
-            birthday_date = datetime.strptime(value, "%d.%m.%Y")
+            birthday_date = datetime.strptime(value, "%d.%m.%Y").date()
             super().__init__(birthday_date)
             # Додайте перевірку коректності даних
             # та перетворіть рядок на об'єкт datetime
@@ -84,8 +84,14 @@ class AddressBook(UserDict):
     
     def add_record(self, record):        
         self.data[record.name.value] = record        
-    def find(self, name):                
-        return(self.data[name])
+    
+    def find(self, name):                        
+        print(self.data.keys())
+        if name in self.data.keys():
+            return(self.data[name])            
+        else: 
+            return None
+    
     def delete(self, name):
         self.data.pop(name)
 
@@ -95,13 +101,11 @@ class AddressBook(UserDict):
             rec = rec + str(record) + '; \n'
         return rec
     
-    def get_upcoming_birthdays(users):
-        current_date = datetime.today().date()
-        # current_date = datetime(year=2024, month=12, day=30).date()    
+    def get_upcoming_birthdays(self):
+        current_date = datetime.today().date()        
         congratulation_dates = []
-        for user in users:
-            user_birthday=datetime.strptime(user["birthday"], "%Y.%m.%d").date()
-            user_birthday_this_year = datetime(year=current_date.year, month=user_birthday.month, day=user_birthday.day)
+        for name, record in self.items():                        
+            user_birthday_this_year = datetime(year=current_date.year, month=record.birthday.value.month, day=record.birthday.value.day)
             user_birthday_this_year = user_birthday_this_year.date()
             if (current_date.month == 12 and user_birthday_this_year.month == 1):            
                 if (current_date.year % 4 == 0):
@@ -117,30 +121,87 @@ class AddressBook(UserDict):
                     congratulation_date = user_birthday_this_year + timedelta(days = 2)
                 elif(user_birthday_this_year.weekday() == 6):
                     congratulation_date = user_birthday_this_year + timedelta(days = 1)                                        
-                congratulation_date = congratulation_date.strftime("%Y.%m.%d")
-                congratulation_dates.append({"name": user["name"], "congratulation_date":congratulation_date})        
+                congratulation_date = congratulation_date.strftime("%d.%m.%Y")
+                congratulation_dates.append({"name": name, "congratulation_date":congratulation_date})                
         return(congratulation_dates)
 
-# birthday = Birthday('2.21.1973')
-# print(birthday)
-book = AddressBook()
-john_record = Record("John")
-john_record.add_phone("1234567890")
-# john_record.add_birthday("12.01.1987")
-john_record.add_phone("5555555555")
-book.add_record(john_record)
-# jane_record = Record("Jane")
-# jane_record.add_phone("9879547210")
-# book.add_record(jane_record)
-# jan_record = Record("Jan")
-# jan_record.add_phone("9876543219")
-# book.add_record(jan_record)
-john = book.find("John")
-print(john)
-# john.edit_phone("1234567890", "1112223333")
-# john.add_phone("5555555585")
-# john.remove_phone("5555555555")
-# print(john)
-# print(book)
-# book.delete("Jane")
-# print(book)
+def parse_input(user_input):
+    try:
+        cmd, *args = user_input.split()
+        cmd = cmd.strip().lower()
+        return cmd, *args
+    except:
+        return " "    
+    
+def add_contact(args, book: AddressBook):
+    name, phone, *_ = args
+    record = book.find(name)    
+    message = "Contact updated."
+    if record is None:
+        record = Record(name)
+        book.add_record(record)
+        message = "Contact added."
+    if phone:
+        record.add_phone(phone)
+    return message
+
+def change_contact(args, book: AddressBook):
+    name, phone, new_phone = args        
+    if book.find(name):
+        book.find(name).edit_phone(phone, new_phone)
+    else: 
+        raise KeyError(f"Contact with name {name} doesn't exist")
+    return "Contact Updated."
+
+def phones(args, book: AddressBook):
+    name = args            
+    if book.find(name):
+        return book.find(name)
+    else: 
+        raise KeyError(f"Contact with name {name} doesn't exist")
+    
+
+def main():
+    book = AddressBook()
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+
+        elif command == "hello":
+            print("How can I help you?")
+
+        elif command == "add":
+            print(add_contact(args, book))
+            # реалізація
+
+        elif command == "change":
+            print(change_contact(args, book))
+            # реалізація
+
+        elif command == "phone":
+            # реалізація
+            print(phones(args, book))
+
+        elif command == "all":
+            print(book)
+            # реалізація
+
+        # elif command == "add-birthday":
+        #     # реалізація
+
+        # elif command == "show-birthday":
+        #     # реалізація
+
+        # elif command == "birthdays":
+        #     # реалізація
+
+        # else:
+        #     print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
